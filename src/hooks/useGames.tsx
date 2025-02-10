70c51344c0ca7a0e138b833079a56e0bc724984d
Amritpal Singh <amritgagra200@gmail.com>
2025-02-10 22:52:21 +0530

    create a Generic data fetching hook

--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -1,6 +1,4 @@
-import apiClient from "@/services/api-client";
-import { CanceledError } from "axios";
-import { useEffect, useState } from "react";
+import useData from "./useData";
 
 export interface platforms {
   id: number;
@@ -14,36 +12,6 @@ export interface Game {
   parent_platforms: { platform: platforms }[];
   metacritic: number;
 }
-
-interface fetchGameResponse {
-  count: number;
-  results: Game[];
-}
-const useGames = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const controller = new AbortController();
-
-    setLoading(true);
-    apiClient
-      .get<fetchGameResponse>("/games", { signal: controller.signal })
-      .then((res) => {
-        setGames(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-
-    return () => controller.abort();
-  }, []);
-
-  return { games, error, isLoading };
-};
+const useGames = () => useData<Game>("/games");
 
 export default useGames;
--- src/hooks/useGenre.tsx
+++ src/hooks/useGenre.tsx
@@ -1,42 +1,10 @@
-import apiClient from "@/services/api-client";
-import { CanceledError } from "axios";
-import { useEffect, useState } from "react";
+import useData from "./useData";
 
 interface Genre {
   id: number;
   name: string;
 }
 
-interface fetechGenreResponse {
-  count: number;
-  results: Genre[];
-}
-
-const useGenre = () => {
-  const [genres, setGenres] = useState<Genre[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const controller = new AbortController();
-
-    setLoading(true);
-    apiClient
-      .get<fetechGenreResponse>("/genres", { signal: controller.signal })
-      .then((res) => {
-        setGenres(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-
-    return () => controller.abort();
-  }, []);
-
-  return { genres, error, isLoading };
-};
+const useGenre = () => useData<Genre>("/genres");
 
 export default useGenre;
